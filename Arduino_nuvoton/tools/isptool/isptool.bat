3de31aae6c7a71ce886a349591202621e19d6175
HLLIN6 <HLLIN6@nuvoton.com>
2024-12-31 11:27:50 +0800

    Update ISP tool to support delay function. Remove unuse files(Nxx, Ixx).

--- Arduino_nuvoton/tools/isptool/isptool.bat
+++ Arduino_nuvoton/tools/isptool/isptool.bat
@@ -4,11 +4,11 @@ set str=%str:~0,2%
 %str%
 cd %1
 %1\isp_command_line.exe -l
-sleep
+%1\isp_command_line.exe -t 1
 %1\isp_command_line.exe -a %2
-sleep 
+%1\isp_command_line.exe -t 1
 %1\isp_command_line.exe -j
-sleep_q 
+%1\isp_command_line.exe -t 1
 exit 0
 
 
